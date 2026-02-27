1aadbdda4a665a6fc941c9ce49044a0789c0220d
Louistrash <patricknieborg@me.com>
2026-02-27 16:21:04 +0100

    Web export: 10 enemies, sound icon drawn, ViewToggle top-right, Gallery button fixed

--- web/index.service.worker.js
+++ web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771518721|3667511614';
+const CACHE_VERSION = '1772205631|39693790';
 /** @type {string} */
 const CACHE_PREFIX = 'Soccer Arena Cla-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
